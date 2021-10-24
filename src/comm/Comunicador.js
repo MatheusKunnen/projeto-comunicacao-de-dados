7e006c3710483c25241aaf0c130272a250ddec6f
MatheusKunnen <matheuskl@accionproactiva.com>
2021-10-23 23:40:32 -0300

    Ajuste no comunicador, para conexões externas

--- src/comm/Comunicador.js
+++ src/comm/Comunicador.js
@@ -14,6 +14,7 @@ const criarServidor = (
   const server = net.createServer(function (s) {
     console.log(`Socket ${host}:${porta} iniciado`);
     socket = s;
+    socket.setEncoding('utf-8');
     running = true;
     // Controla eventos
     socket.on('data', (data) => {
@@ -35,7 +36,7 @@ const criarServidor = (
   });
 
   // Inicia servidor
-  server.listen(porta, host);
+  server.listen(porta);
 
   const close = () => {
     if (socket !== null) socket.close();
@@ -56,13 +57,7 @@ const connectarServidor = (
 ) => {
   var client = new net.Socket();
   let running = false;
-
-  client.connect(porta, host, function () {
-    console.log('Connected');
-    running = true;
-    // client.write('Hello, server! Love, Client.');
-  });
-
+  client.setNoDelay(true);
   client.on('data', function (data) {
     // console.log('Received: ' + data);
     callback(data);
@@ -74,6 +69,12 @@ const connectarServidor = (
     running = false;
   });
 
+  client.connect({ port: porta, host }, function () {
+    console.log('Connected');
+    running = true;
+    // client.write('Hello, server! Love, Client.');
+  });
+
   const close = () => {
     running = false;
     client.destroy();
@@ -82,7 +83,9 @@ const connectarServidor = (
   const isRunning = () => running;
 
   const write = (msg) => {
-    client.write(`${msg}\n`);
+    // client.write('@!>\n');
+    client.write(`${msg}\r\n\r\n`);
+    // client.end();
   };
   return {
     close,
