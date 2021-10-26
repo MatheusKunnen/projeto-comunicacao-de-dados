787c892b81712f9eb5a2cd01213edc7decaefcd6
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2021-10-26 00:40:31 -0300

    Melhora de confiabilidade do comunicador

--- src/comm/Comunicador.js
+++ src/comm/Comunicador.js
@@ -2,44 +2,59 @@ const net = window.require('net');
 
 const PORTA_DEFAULT = 5100;
 const HOST_DEFAULT = 'localhost';
-const dummy = (data) => console.log(data);
+const dummy = (data) => {}; //console.log(data);
 
 const criarServidor = (
   host = HOST_DEFAULT,
   porta = PORTA_DEFAULT,
-  callback = dummy
+  callback = dummy,
+  statusChangeCallback = dummy
 ) => {
   let socket = null;
   let running = false;
+  statusChangeCallback(running);
   const server = net.createServer(function (s) {
     console.log(`Socket ${host}:${porta} iniciado`);
     socket = s;
-    socket.setEncoding('utf-8');
     running = true;
+    statusChangeCallback(running);
     // Controla eventos
     socket.on('data', (data) => {
-      // console.log(data.toString('utf-8'));
       // Envia informação para o callback
       callback(data);
       socket.write(data);
     });
     socket.on('close', () => {
       running = false;
+      statusChangeCallback(running);
       console.log(`Socket ${host}:${porta} fechado.`);
     });
     socket.on('error', function (err) {
       console.error(`ERROR: ${err}`);
-      console.error(new Error().stack);
+      running = false;
+      socket.destroy();
+      statusChangeCallback(running);
     });
-    // Faz broadcast de toda informação recebida.
-    // socket.pipe(socket);
+    socket.resume();
   });
-
-  // Inicia servidor
-  server.listen(porta);
+  server.on('error', function (err) {
+    console.error(`ERROR: ${err}`);
+    // console.error(new Error().stack);
+    running = false;
+    socket.destroy();
+    statusChangeCallback(running);
+  });
+  try {
+    // Inicia servidor
+    server.listen(porta);
+  } catch (err) {
+    console.error(err);
+  }
 
   const close = () => {
-    if (socket !== null) socket.close();
+    console.log('Fechando Servidor..');
+    running = false;
+    statusChangeCallback(false);
     server.close();
   };
   const isRunning = () => running;
@@ -53,39 +68,41 @@ const criarServidor = (
 const connectarServidor = (
   host = HOST_DEFAULT,
   porta = PORTA_DEFAULT,
-  callback = dummy
+  callback = dummy,
+  statusChangeCallback = dummy
 ) => {
   var client = new net.Socket();
+
   let running = false;
   client.setNoDelay(true);
+
   client.on('data', function (data) {
-    // console.log('Received: ' + data);
     callback(data);
-    //client.destroy(); // kill client after server's response
   });
 
   client.on('close', function () {
-    console.log('Connection closed');
+    console.log('Conexão fechada');
     running = false;
+    statusChangeCallback(running);
   });
-
+  client.setTimeout(3000);
   client.connect({ port: porta, host }, function () {
-    console.log('Connected');
+    console.log('Conectado');
     running = true;
-    // client.write('Hello, server! Love, Client.');
+    statusChangeCallback(running);
   });
 
   const close = () => {
+    console.log('Fechando Cliente..');
     running = false;
+    statusChangeCallback(running);
     client.destroy();
   };
 
   const isRunning = () => running;
 
   const write = (msg) => {
-    // client.write('@!>\n');
-    client.write(`${msg}\r\n\r\n`);
-    // client.end();
+    client.write(`${msg}\r\n`);
   };
   return {
     close,
